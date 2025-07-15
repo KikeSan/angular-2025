660be9025a1dd4f75c2ee4e1c0e7bae9ab338661
Enrique Sanchez <enrique.sanchez@flowww.com>
2025-07-15 16:04:19 +0200

    signals

--- 02-bases/src/app/app.component.ts
+++ 02-bases/src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, signal} from '@angular/core';
 
+// @ts-ignore
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Kike Sanchez';
+  counter:number = 0;
+  counterSignal = signal(0)
+
+  constructor(){}
+
+  increaseBy(value: number){
+    this.counter += value;
+    this.counterSignal.update(current=> current + value)
+  }
+
+  resetCounter(){
+    this.counter = 0;
+    this.counterSignal.set(0)
+  }
+
 }
