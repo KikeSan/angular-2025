e7aa045c82f4ba1e6ce7e83677b20951b2c0e9a6
enrique.sanchez <kikin1601@hotmail.com>
2025-11-18 23:49:08 +0100

    service

--- 02-bases/src/app/pages/dragonball-super/dragonball-super.component.ts
+++ 02-bases/src/app/pages/dragonball-super/dragonball-super.component.ts
@@ -1,12 +1,9 @@
 import {Component, computed, signal} from '@angular/core';
 import {CharacterListComponent} from '../../components/dragonball/character-list/character-list.component';
 import {CharacterAddComponent} from '../../components/dragonball/character-add/character-add.component';
+import {DragonballService} from '../../services/dragonball.service';
+import {inject} from '@angular/core';
 
-interface Character {
-  id: number;
-  name: string;
-  power: number;
-}
 
 @Component({
   selector: 'app-dragonball-super',
@@ -19,16 +16,7 @@ interface Character {
 })
 export class DragonballSuperComponent {
 
-  name = signal('');
-  power = signal(0);
+  public dragonballService = inject(DragonballService);
 
-  characters = signal<Character[]>([
-    {id: 1, name: 'Goku', power: 9001},
-    {id: 2, name: 'Vegeta', power: 8000}
-  ]);
-
-  addCharacter(character: Character){
-    this.characters.update(characters => [...characters, character]);
-  }
 
 }
