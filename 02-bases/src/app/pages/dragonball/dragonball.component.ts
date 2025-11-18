aabf3849318dc83db86ff120e0fa5b255f207fbc
enrique.sanchez <kikin1601@hotmail.com>
2025-11-18 21:43:17 +0100

    @if

--- 02-bases/src/app/pages/dragonball/dragonball.component.ts
+++ 02-bases/src/app/pages/dragonball/dragonball.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, computed, signal} from '@angular/core';
+import {NgClass} from '@angular/common';
+
+interface Character {
+  id: number;
+  name: string;
+  power: number;
+}
 
 @Component({
   selector: 'app-dragonball',
-  imports: [],
   templateUrl: './dragonball.component.html',
   styleUrl: './dragonball.component.css'
 })
 export class DragonballComponent {
+
+  characters = signal<Character[]>([
+    {id: 1, name: 'Goku', power: 9001},
+    {id: 2, name: 'Vegeta', power: 8000},
+    {id: 3, name: 'Trunks', power: 7000},
+    {id: 4, name: 'Yamcha', power: 500}
+  ]);
+
+  powerClasses = computed(()=>{
+    return{
+      'text-danger': true,
+    }
+  })
 
 }
