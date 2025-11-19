886784363abe81fbd597e8549a315a132a7aa0ee
enrique.sanchez <kikin1601@hotmail.com>
2025-11-19 21:42:25 +0100

    effect()

--- 02-bases/src/app/services/dragonball.service.ts
+++ 02-bases/src/app/services/dragonball.service.ts
@@ -1,4 +1,4 @@
-import {Injectable, signal} from '@angular/core';
+import {effect, Injectable, signal} from '@angular/core';
 import {Character} from '../interfaces/character.interface';
 
 @Injectable({providedIn: 'root'})
@@ -8,6 +8,11 @@ export class DragonballService {
     {id: 1, name: 'Goku', power: 9001},
     {id: 2, name: 'Vegeta', power: 8000}
   ]);
+
+  saveToLocalStorage = effect(()=>{
+    localStorage.setItem('characters', JSON.stringify(this.characters()));
+    console.log(`Character count: ${this.characters().length}`)
+  });
 
   addCharacter(character: Character){
     this.characters.update(characters => [...characters, character]);
