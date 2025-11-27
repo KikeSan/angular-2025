a700ce157595430f5cf0f6dfc9c05c72279f6a6d
enrique.sanchez <kikin1601@hotmail.com>
2025-11-27 22:14:02 +0100

    router

--- 03-gifs-app/src/app/gifs/components/gifs-side-menu/gifs-side-menu-options/gifs-side-menu-options.ts
+++ 03-gifs-app/src/app/gifs/components/gifs-side-menu/gifs-side-menu-options/gifs-side-menu-options.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive} from '@angular/router';
+
+interface MenuOption {
+  label: string;
+  sublabel: string;
+  route: string;
+  icon: string;
+}
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './gifs-side-menu-options.html',
   styleUrl: './gifs-side-menu-options.css',
 })
 export class GifsSideMenuOptions {
-
+  menuOptions:MenuOption[] = [
+    {
+      label: 'Trending',
+      sublabel: 'Gifs de la semana',
+      route: '/dashboard/trending',
+      icon: 'fa-solid fa-chart-line'
+    },
+    {
+      label: 'Search',
+      sublabel: 'Buscar gifs',
+      route: '/dashboard/search',
+      icon: 'fa-solid fa-magnifying-glass'
+    },
+  ]
 }
